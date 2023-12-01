d2060c96ac97bd1264779cc0c92a7bb35dd81e24
Tomi Markus Alber <tomimarkus.alber@egrupp.ee>
2023-12-01 13:59:23 +0200

    Fix publish package config

--- dist/index.test-d.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tsd_1 = require("tsd");
-var index_js_1 = require("./index.js");
-var text = "Lorem ipsum dolor sit amet, //sindresorhus.com consectetuer adipiscing http://yeoman.io elit.";
-(0, tsd_1.expectType)((0, index_js_1.getUrls)(text));
-(0, tsd_1.expectType)((0, index_js_1.getUrls)(text, { extractFromQueryString: true }));
-(0, tsd_1.expectType)((0, index_js_1.getUrls)(text, { exclude: ["foo"] }));
-(0, tsd_1.expectType)((0, index_js_1.getUrls)(text, { defaultProtocol: "http" }));
